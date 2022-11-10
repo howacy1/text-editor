966042ff2bffcdf833899cbdee9332a84e1e3bc7
Cy Howard <cyhoward47@gmail.com>
2022-11-09 23:05:57 -0600

    finished untested

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+export const initdb = async () =>
+  openDB('jate_db', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const contactDb = await openDB('jate_db', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('Database updated', result.value);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('jate_db', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+}
 
 initdb();
